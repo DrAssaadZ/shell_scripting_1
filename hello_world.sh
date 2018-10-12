f28d367f3b052648ca3d7dfe9c0b838a06dbfbb8
assaad <las9ayne_215@hotmail.com>
2018-10-12 21:11:31 +0000

    first try

---
+++ hello_world.sh
@@ -0,0 +1,10 @@
+#hello this is my first shell script
+echo "hello world"
+echo "now m gonna show the files of the xecuted in folder"
+ls -l
+echo "okey, now clear the screen"
+read x
+clear
+echo "enter your name"
+read _username
+echo " so ur name is $_username, nice to meet you ^^"
