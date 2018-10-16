24ce0895d434f7492f9f506907acdbbb79c42e89
assaad <las9ayne_215@hotmail.com>
2018-10-16 12:02:56 +0000

    minnor modifications

--- hello_world.sh
+++ hello_world.sh
@@ -8,3 +8,8 @@ clear
 echo "enter your name"
 read _username
 echo " so ur name is $_username, nice to meet you ^^"
+echo here we go again
+echo -e "\e[1;42m this is green background text \e[0m \t hello" #didnt work for me!
+_var="hello there"
+echo ${#_var} #length of the variable _var
+
